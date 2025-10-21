716f6fa8dc2a7d0a68e6a0564ec5d302f00680f8
o-seoj <tjwjd010@naver.com>
2025-10-21 15:58:55 +0900

    test

--- src/main/java/kr/co/kmarket/security/SecurityConfig.java
+++ src/main/java/kr/co/kmarket/security/SecurityConfig.java
@@ -66,7 +66,7 @@ public class SecurityConfig {
                 .userInfoEndpoint(userInfo -> userInfo
                         .userService(customOauth2UserService)
                 )
-                .successHandler(successHandler)
+                .defaultSuccessUrl("/")
                 .failureUrl("/member/login?error=true")
         );
 
